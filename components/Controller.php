86d228a8d741bc992f3002a7b39e321eb644d20e
Pavel Veselov <pvsaintpe@icloud.com>
2018-11-23 00:57:22 +0300

    fix

--- components/Controller.php
+++ components/Controller.php
@@ -3,10 +3,14 @@
 namespace pvsaintpe\search\components;
 
 use pvsaintpe\search\interfaces\SearchInterface;
+use pvsaintpe\search\traits\SearchTrait;
 use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
+use yii\helpers\Json;
 use yii\web\Controller as WebController;
 use yii\web\Application as WebApplication;
 use Yii;
+use yii\web\Response;
 
 /**
  * Class Controller
@@ -22,6 +26,29 @@ class Controller extends WebController
     protected $rule = null;
 
     /**
+     * @inheritdoc
+     */
+    public function afterAction($action, $result)
+    {
+        $result = parent::afterAction($action, $result);
+        if (is_string($result) && Yii::$app->getRequest()->getIsAjax()
+            && (Yii::$app->getResponse()->format == Response::FORMAT_HTML)
+        ) {
+            $view = $this->getView();
+            if ($view->title) {
+                $title = Json::htmlEncode($view->title);
+                $result .= <<<JS
+<script>
+jQuery('#main-modal').find('.modal-title').html({$title});
+jQuery('#main-modal').find('.box').removeClass().children().removeClass();
+</script>
+JS;
+            }
+        }
+        return $result;
+    }
+
+    /**
      * @param null|string $searchClass
      * @return ActiveRecord|SearchInterface
      */
@@ -30,15 +57,34 @@ class Controller extends WebController
         if (!$searchClass) {
             $searchClass = $this->searchClass;
         }
-        /** @var ActiveRecord|SearchInterface $searchModel */
+
+        /** @var ActiveRecord|SearchInterface|SearchTrait $searchModel */
         $searchModel = new $searchClass();
-        $searchModel->load(Yii::$app->request->queryParams);
+        $searchModel->load($this->getParams());
+
         $searchModel->setView($this->getView());
         $searchModel->setViewPath('/' . $this->module->id . '/' . $this->id . '/view/');
         $searchModel->setUpdatePath('/' . $this->module->id . '/' . $this->id . '/update/');
         $searchModel->setPermissionPrefix($this->getPermissionPrefix());
 
         return $searchModel;
+    }
+
+    /**
+     * @return array
+     * @throws \yii\base\InvalidConfigException
+     */
+    protected function getParams()
+    {
+        $params = Yii::$app->request->getBodyParams();
+        if ($params) {
+            Yii::$app->session->set('bodyParams', $params);
+        }
+        $params = ArrayHelper::merge($params, Yii::$app->request->queryParams);
+        if (isset($params['params']) && is_string($params['params'])) {
+            $params = ArrayHelper::merge($params, unserialize($params['params']));
+        }
+        return $params;
     }
 
     /**
@@ -61,10 +107,10 @@ class Controller extends WebController
      */
     public function render($view, $params = array())
     {
-        return parent::render($view, array_merge($params, [
+        return parent::render($view, array_merge([
             'disableAttributes' => [],
             'permissionPrefix' => $this->getPermissionPrefix()
-        ]));
+        ], $params));
     }
 
     /**
@@ -75,10 +121,10 @@ class Controller extends WebController
      */
     public function renderAjax($view, $params = array())
     {
-        return parent::renderAjax($view, array_merge($params, [
+        return parent::renderAjax($view, array_merge([
             'disableAttributes' => [],
             'permissionPrefix' => $this->getPermissionPrefix()
-        ]));
+        ], $params));
     }
 
     /**
